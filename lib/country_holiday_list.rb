84532bc945b94cbe8786fe65c4bdba464f434217
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-12-19 15:34:08 +0000

    initial commit

---
+++ lib/country_holiday_list.rb
@@ -0,0 +1,6 @@
+require "country_holiday_list/version"
+
+module CountryHolidayList
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/country_holiday_list/version.rb
@@ -0,0 +1,3 @@
+module CountryHolidayList
+  VERSION = "0.1.0"
+end
