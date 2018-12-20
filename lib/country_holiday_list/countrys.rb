9d72f3b6d6780b89d41f038f18582bf64cafa4d3
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-12-20 21:46:13 +0000

    initial commit

--- lib/country_holiday_list/countrys.rb
+++ lib/country_holiday_list/countrys.rb
@@ -1,1 +1,1 @@
-require_relevant ""
+require_relevant "scraper"
--- lib/country_holiday_list/scraper.rb
+++ lib/country_holiday_list/scraper.rb
@@ -1,13 +1,23 @@
 require "nokogiri"
 require "open-uri"
+require "pry"
 
 doc = Nokogiri::HTML(open("https://www.calendarindex.com/"))
-year = Time.new.year
 
-def holiday_info(code)
+def holiday(code)
+  year = Time.new.year
 
-  doc = Nokogiri::HTML(open("https://www.calendarindex.com/holidays/#{@year}/#{code}"))
-  doc.css("tr").collect do |holiday|
+  doc = Nokogiri::HTML(open("https://www.calendarindex.com/holidays/#{year}/#{code}"))
+  holiday_hash = {}
 
+  doc.css("tr").collect do |info|
 
+    holiday_hash << info.split("/a")
+    # holiday_hash ={
+    #   :name => info[0]
+    #   :day => info[1]
+    #   :date => info[2]
+    #   :type => info[3]
+    # }
   end
+end
