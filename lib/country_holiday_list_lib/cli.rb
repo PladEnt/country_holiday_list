9d618b323edc571510a11374f232baf0927d9280
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-12-31 20:47:19 +0000

    add def welcome and where_to? to CLI.rb

--- lib/country_holiday_list_lib/cli.rb
+++ lib/country_holiday_list_lib/cli.rb
@@ -6,7 +6,35 @@ class CLI
   def initialize(user)
     @user = user
   end
+
+  def welcome
+    puts "|Welcome to Country Holiday List|"
+    puts "|-------------------------------|"
+    puts "|Please enter the countrys code |"
+    puts "|you want to know about.        |"
+    puts "|-------------------------------|"
+    puts "|or type 'list' to get a list   |"
+    puts "|of countries.                  |"
+    puts "|-------------------------------|"
+    puts "|type 'exit' to end the program |"
+    puts " "
+
+  where_to?
+  end
+
+  def where_to?
+
+    imput = gets.chomp.strip
+
+    if imput == "list"
+
+    else
+      puts " "
+      country_info(imput)
+    end
+  end
 end
 
 
 CLI.new("josh")
+CLI.welcome
